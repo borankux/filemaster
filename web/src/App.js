726a503c22752f282c5e295d1243034d3c863205
FrankWindows <mirzatsoft@163.com>
2021-07-08 01:36:19 +0800

    now it has a face

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Link, Route} from "react-router-dom";
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class  App extends Component{
+    state = {
+        gists: null
+    }
+
+    componentDidMount() {
+        fetch("https://api.github.com/gists")
+            .then(res => res.json())
+            .then(gists => {
+                this.setState({gists})
+            })
+    }
+
+    render() {
+        const {gists} = this.state
+        return (
+            <div className="App">
+                <Router>
+                <div className="info-box">
+                    <div className="info">
+                        <h3>Scanners</h3>
+                        <hr/>
+                        <span>1</span>
+                    </div>
+                    <ul>
+                        {gists ? (gists.map(gist => (
+                            <li key={gist.id}>
+                                <Link to={`/g/${gist.id}`}>
+                                    {gist.description}
+                                </Link>
+                            </li>
+                            ))) :
+                            (<div>loading...</div>)}
+                    </ul>
+                </div>
+
+                <div className="page">
+                    {gists && (
+                        <Route path="/g/:gistId" render={({match}) => (
+                        <Gist gist={gists.find(g => g.id === match.params.gistId)} />
+                        )}/>
+                    )}
+                </div>
+                </Router>
+            </div>
+        );
+    }
+}
+
+
+const Gist = ({ gist }) => {
+    return (
+        <div>
+            <h3>{gist.description}</h3>
+            <div>
+            </div>
+        </div>
+    )
 }
 
 export default App;
