74c71b713dc9ad47b07a753919f2859e3899388e
FrankWindows <mirzatsoft@163.com>
2021-07-09 00:42:30 +0800

    add command feed

--- server/app.go
+++ server/app.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/borankux/filemaster/server/boot"
 	"github.com/borankux/filemaster/server/config"
 	"github.com/borankux/filemaster/server/db"
 	"github.com/borankux/filemaster/server/router"
 	"github.com/gin-gonic/gin"
+	"os"
 )
 
 func main() {
+	env := flag.String("e", "dev", "")
+
+	flag.Usage = func() {
+		fmt.Println("Usage: server -e {mode}")
+		os.Exit(1)
+	}
+	flag.Parsed()
+
 	gin.SetMode(gin.DebugMode)
-	config.Init("dev")
+	config.Init(*env)
 	db.Init()
 	boot.Migrate()
 	router.Init()
